b6875e0e845cca6c0f2266ece335a4f9f5013ea4
SOURAV <56021891+hattymatty1@users.noreply.github.com>
2020-11-07 11:29:19 +0530

    new with watermarks

--- Flappy_Bird/Flappy-Bird-day.py
+++ Flappy_Bird/Flappy-Bird-day.py
@@ -13,7 +13,8 @@ def create_pipe():
 def move_pipes(pipes):
     for pipe in pipes:
         pipe.centerx -= 5
-    return pipes
+    visible_pipes = [pipe for pipe in pipes if pipe.right > -50]
+    return visible_pipes
 
 def draw_pipes(pipes):
     for pipe in pipes:
@@ -24,12 +25,16 @@ def draw_pipes(pipes):
             screen.blit(flip_pipe,pipe)
 
 def check_collision(pipes):
+    global can_score
+    
     for pipe in pipes:
         if bird_rect.colliderect(pipe):
             death_sound.play()
+            can_score = True
             return False
 
     if bird_rect.top <= -100 or bird_rect.bottom >= 700:
+        can_score = True
         return False
 
     return True
@@ -45,9 +50,18 @@ def bird_animation():
 
 def score_display(game_state):
     if game_state == 'main_game':
+        group_surface = game_font.render(f'Group: {group_name}',True,(255,255,255))
+        group_rect = group_surface.get_rect(center = (288,50))
+        screen.blit(group_surface,group_rect)
+        
         score_surface = game_font.render(str(int(score)),True,(255,255,255))
         score_rect = score_surface.get_rect(center = (288,100))
         screen.blit(score_surface,score_rect)
+
+        high_score_surface = game_font.render(f'H: {int(high_score)}',True,(255,255,255))
+        high_score_rect = high_score_surface.get_rect(center = (500,100))
+        screen.blit(high_score_surface,high_score_rect)
+
     if game_state == 'game_over':
         score_surface = game_font.render(f'Score: {int(score)}',True,(255,255,255))
         score_rect = score_surface.get_rect(center = (288,40))
@@ -57,14 +71,30 @@ def score_display(game_state):
         high_score_rect = high_score_surface.get_rect(center = (288,100))
         screen.blit(high_score_surface,high_score_rect)
 
+        developer_surface = game_font.render(f'by: {developer_name}',True,(255,255,255))
+        developer_rect = developer_surface.get_rect(center = (288,275))
+        screen.blit(developer_surface,developer_rect)
+
 def update_score(score, high_score):
     if score > high_score:
         high_score = score
     return high_score
 
+def pipe_score_check():
+    global score,can_score
+    
+    if pipe_list:
+        for pipe in pipe_list:
+            if 95 < pipe.centerx < 105 and can_score:
+                score += 1
+                score_sound.play()
+                can_score = False
+            if pipe.centerx < 0:
+                can_score = True
+
 gameIcon = pygame.image.load('E:/Flappy_Bird/favicon.ico')
 pygame.display.set_icon(gameIcon)
-pygame.mixer.pre_init(frequency = 44100, size = 16, channels = 1, buffer = 512)
+#pygame.mixer.pre_init(frequency = 44100, size = 16, channels = 1, buffer = 512)
 pygame.init()
 pygame.display.set_caption('Flappy Bird')
 screen = pygame.display.set_mode((576,800))
@@ -77,6 +107,7 @@ bird_movement = 0
 game_active = True
 score = 0
 high_score = 0
+can_score = True
 
 bg_surface = pygame.image.load('E:/Flappy_Bird/assets/background-day.png').convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
@@ -85,9 +116,9 @@ floor_surface = pygame.image.load('E:/Flappy_Bird/assets/base.png').convert()
 floor_surface = pygame.transform.scale2x(floor_surface)
 floor_x_pos = 0
 
-bird_downflap = pygame.transform.scale2x(pygame.image.load('E:/Flappy_Bird/assets/bluebird-downflap.png').convert_alpha())
-bird_midflap = pygame.transform.scale2x(pygame.image.load('E:/Flappy_Bird/assets/bluebird-midflap.png').convert_alpha())
-bird_upflap = pygame.transform.scale2x(pygame.image.load('E:/Flappy_Bird/assets/bluebird-upflap.png').convert_alpha())
+bird_downflap = pygame.transform.scale2x(pygame.image.load('E:/Flappy_Bird/assets/yellowbird-downflap.png').convert_alpha())
+bird_midflap = pygame.transform.scale2x(pygame.image.load('E:/Flappy_Bird/assets/yellowbird-midflap.png').convert_alpha())
+bird_upflap = pygame.transform.scale2x(pygame.image.load('E:/Flappy_Bird/assets/yellowbird-upflap.png').convert_alpha())
 bird_frames = [bird_downflap,bird_midflap,bird_upflap]
 bird_index = 0
 bird_surface = bird_frames[bird_index]
@@ -100,12 +131,15 @@ pygame.time.set_timer(BIRDFLAP,200)
 bird_surface = pygame.transform.scale2x(bird_surface)
 bird_rect = bird_surface.get_rect(center = (100,350))'''
 
-pipe_surface = pygame.image.load('E:/Flappy_Bird/assets/pipe-green.png')
+developer_name = 'hattymatty Co. ltd'
+group_name = 'Sourav & Sushant'
+
+pipe_surface = pygame.image.load('E:/Flappy_Bird/assets/pipe-green.png').convert()
 pipe_surface = pygame.transform.scale2x(pipe_surface)
 pipe_list = []
 SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE,1200)
-pipe_height = [400,500,600]
+pipe_height = [300,350,400,450,500,550,600]
 
 game_over_surface = pygame.transform.scale2x(pygame.image.load('E:/Flappy_Bird/assets/message.png').convert_alpha())
 game_over_rect = game_over_surface.get_rect(center = (280,400))
@@ -162,12 +196,11 @@ while True:
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
 
-        score += 0.01
+        #Score
+
+        pipe_score_check()
         score_display('main_game')
-        score_sound_countdown -= 1
-        if score_sound_countdown <= 0:
-            score_sound.play()
-            score_sound_countdown = 100
+        
     else:
         screen.blit(game_over_surface,game_over_rect)
         high_score = update_score(score,high_score)
